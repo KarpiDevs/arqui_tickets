76160857ee761bd37786ff43c8da0a5cabab8e6f
Karpi19 <durebrian01@gmail.com>
2024-11-10 22:43:57 -0300

    Commit inicial

---
+++ src/main/java/py/com/arquitickets/ArquiTicketsApplication.java
@@ -0,0 +1,13 @@
+package py.com.arquitickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/com/arquitickets/ArquiTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.arquitickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
